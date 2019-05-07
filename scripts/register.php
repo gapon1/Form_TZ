bbf6172a37bdbaf85f4b7762449baafa6b5c5de7
Vitaliy <gapon007@ukr.net>
2019-05-07 14:36:11 +0300

    Create verification for img

--- scripts/register.php
+++ scripts/register.php
@@ -1,40 +1,51 @@
 <?php
 
 require_once "db.php";
-require_once "upload-avatar.php";
 
-$login = trim($_POST['login']);
-$password = trim($_POST['password']);
-$email = trim($_POST['email']);
-$filename = UploaddImg($_FILES['image']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-if (!empty($login) && !empty($password) && !empty($email)) {
+    function validate_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 
-    //==== Check if user Exists in user table ======
-    $sql_check = 'SELECT EXISTS( SELECT email FROM users WHERE email = :email)';
-    $stmt_check = $pdo->prepare($sql_check);
-    $stmt_check->execute([':email' => $email]);
-    if ($stmt_check->fetchColumn()) {
-        $errorMessage = "User with choose email already EXIST !";
+    $login = validate_input($_POST['login']);
+    $password = validate_input($_POST['password']);
+    $email = validate_input($_POST['email']);
+    $filename = UploaddImg($_FILES['image']);
+
+
+
+    if (!empty($login) && !empty($password) && !empty($email)) {
+
+        //==== Check if user Exists in user table ======
+        $sql_check = 'SELECT EXISTS( SELECT email FROM users WHERE email = :email)';
+        $stmt_check = $pdo->prepare($sql_check);
+        $stmt_check->execute([':email' => $email]);
+        if ($stmt_check->fetchColumn()) {
+            $errorMessage = "User with choose email already EXIST !";
+            include '../errors.php';
+            exit();
+        }
+
+        $password = password_hash($password, PASSWORD_DEFAULT);
+
+        //============ Insert gets user data to DB ==========
+        $sql = 'INSERT INTO users(login, email, password, image) VALUES(:login, :email, :password, :image)';
+        $params = [':login' => $login, ':email' => $email, ':password' => $password, ':image' => $filename];
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($params);
+
+        header("Location: ../login-form.html");
+        $errorMessage = "Registration Success";
+        include '../errors.php';
+        exit();
+
+    } else {
+        $errorMessage = "Please fill all columns";
         include '../errors.php';
         exit();
     }
-
-    $password = password_hash($password, PASSWORD_DEFAULT);
-
-    //============ Insert gets user data to DB ==========
-    $sql = 'INSERT INTO users(login, email, password, image) VALUES(:login, :email, :password, :image)';
-    $params = [':login' => $login, ':email' => $email, ':password' => $password,  ':image' => $filename];
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute($params);
-
-    header("Location: ../login-form.html");
-    $errorMessage = "Registration Success";
-    include '../errors.php';
-    exit();
-
-} else {
-    $errorMessage = "Please fill all columns";
-    include '../errors.php';
-    exit();
 }
--- scripts/upload-avatar.php
+++ scripts/upload-avatar.php
@@ -1,15 +1,47 @@
 <?php
-
+error_reporting(0);
 require_once "register.php";
 
 function UploaddImg($image)
 {
+    //======== Set name for uploads image ========
     $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
     $filename = uniqid() . "." . $extension;
+
+    //========= Verify image type  ================
+    function get_image_info($filename = NULL)
+    {
+        if (!is_file($filename)) return false;
+
+        if (!$data = getimagesize($filename) or !$filesize = filesize($filename)) return false;
+
+        $extensions = [
+            1 => 'gif',
+            2 => 'jpg',
+            3 => 'png'
+        ];
+
+        $result = array('width' => $data[0],
+            'height' => $data[1],
+            'extension' => $extensions[$data[2]],
+            'size' => $filesize,
+            'mime' => $data['mime']);
+
+        return $result;
+    }
+
+    $valid_extensions = array('gif', 'jpg', 'png');
+
+    if (!$image_info = get_image_info($_FILES['image']['tmp_name']) or !in_array($image_info['extension'], $valid_extensions)) {
+        $errorMessage = "Wrong Image type, yor cal load just png, jpg or gif";
+        include '../errors.php';
+        exit();
+    }
 
     move_uploaded_file($image['tmp_name'], "../uploads/" . $filename);
 
     return $filename;
+
 }
 
 
